9d24e619fdf2b0e27d1e50a008a75acf14efbd52
Murugan Amir <murugan.learning@gmail.com>
2019-08-23 01:30:16 +0100

    Refactoring

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,15 +4,17 @@ import App from '../App';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+})
+
 it('shows a comment box', () => {
-    const wrapped = shallow(<App />);
-    
     expect(wrapped.find(CommentBox).length).toEqual(1);
 })
 
 it('shows a commentlist ', () => {
-    const wrapped = shallow(<App />);
-
     expect(wrapped.find(CommentList).length).toEqual(1);
 })
 
