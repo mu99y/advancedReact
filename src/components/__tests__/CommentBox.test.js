4ce19fc0d7c3cdd16c89a211879c3bf5b2592e72
Murugan Amir <murugan.learning@gmail.com>
2019-08-23 02:13:18 +0100

    Simulating change event, providing fake event object, force comp to update, retrieving prop to assert

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -13,6 +13,12 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 })
 
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {target:{value:'test'}});
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('test');
+})  
+
 afterEach(() => {
     wrapped.unmount();
 })
