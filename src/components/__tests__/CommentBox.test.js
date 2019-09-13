7ab86801323ee3e7fd6d6c33ed7e33fd17cfd0d3
Murugan Amir <murugan.learning@gmail.com>
2019-09-13 19:18:43 +0100

    Integration test with setTimeOut

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,9 +9,9 @@ beforeEach(() => {
     wrapped =  mount(<Root><CommentBox /></Root>);
 })
 
-it('has a text area and a button', () => {
+it('has a text area and two buttons', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(2);
 })
 
 describe('the text area', () => {
